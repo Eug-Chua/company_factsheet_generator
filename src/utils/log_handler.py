8eb57d0fffde68fc5eb289e8f52c723535d7b132
eugenechua <eugene.chua.mail@gmail.com>
2025-10-07 14:24:12 +0800

    feat: get file number and check if log file is valid

--- src/utils/log_handler.py
+++ src/utils/log_handler.py
@@ -18,3 +18,18 @@ def _count_lines(self, file_path: Path) -> int:
 def _has_space(self, file_path: Path) -> bool:
     """Checks if file has space"""
     return self._count_lines(file_path) < self.max_lines
+
+def _extract_file_number(self, file_path: Path, base_name:str) ->int :
+    """Extract log file's number"""
+    name = file_path.stem
+    if name == base_name:
+        return 1
+    num_str = file_path.stem.split('_')[-1]
+    if num_str.isdigit():
+        return int(num_str)
+    return -1
+
+def _is_valid_log_file(self, file: Path, base_name: str) -> bool:
+    """Check if log file is valid"""
+    return self._extract_file_number(file, base_name) != -1
+
